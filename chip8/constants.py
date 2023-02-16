364277e672fbd0d354cacd33fca2fe2ccaf06095
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2023-02-16 12:32:13 +1100

    Create constants.py

---
+++ chip8/constants.py
@@ -0,0 +1,24 @@
+import pyglet
+
+# Defines keymap for QWERTY keyboard support
+KEY_MAP = {
+  pyglet.window.key._1: 0x1,
+  pyglet.window.key._2: 0x2,
+  pyglet.window.key._3: 0x3,
+  pyglet.window.key._4: 0xc,
+  pyglet.window.key.Q: 0x4,
+  pyglet.window.key.W: 0x5,
+  pyglet.window.key.E: 0x6,
+  pyglet.window.key.R: 0xd,
+  pyglet.window.key.A: 0x7,
+  pyglet.window.key.S: 0x8,
+  pyglet.window.key.D: 0x9,
+  pyglet.window.key.F: 0xe,
+  pyglet.window.key.Z: 0xa,
+  pyglet.window.key.X: 0,
+  pyglet.window.key.C: 0xb,
+  pyglet.window.key.V: 0xf
+}
+
+# Toggles logging on/off
+LOGGING = False
